232b4a8c87607d0b8cb918aa929ddd6d20322738
slightair <arksutite@gmail.com>
2017-08-02 00:04:30 +0900

    Add version command

--- lib/enoki/cli.rb
+++ lib/enoki/cli.rb
@@ -6,3 +6,13 @@ require 'enoki/cli/init'
 require 'enoki/cli/show'
 require 'enoki/cli/list'
 require 'enoki/cli/generate'
+
+module Enoki
+  class CLI < Thor
+    desc "version", "print enoki version"
+    def version
+      puts Enoki::VERSION
+    end
+    map %w(-v --version) => :version
+  end
+end
